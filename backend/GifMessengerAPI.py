8a520f80a65f94606be1a60e750704749ddd702d
Bine Lipuš <bine.lipus114@gmail.com>
2021-05-22 13:19:46 +0200

    First Commit

---
+++ backend/GifMessengerAPI.py
@@ -0,0 +1,52 @@
+import json
+
+from simple_websocket_server import WebSocketServer, WebSocket
+
+import tenorAPI
+
+
+class SimpleChat(WebSocket):
+    def handle(self):
+        parameters = json.loads(self.data)
+
+        if parameters["type"] == "client hello!":
+            clients[parameters["username"]] = clients[str(self)]
+            clients.pop(str(self))
+
+        if parameters["type"] == "request":
+            self.send_message(tenorAPI.searchForGIFS(parameters["numOfGifs"], parameters["keywords"]))
+            # Get GIFS and send them back to whom requested them.
+
+        if parameters["type"] == "who online":
+            clients_usernames = clients.keys()
+            self.send_message(json.dumps(clients_usernames))
+
+        if parameters["type"] == "send":
+            try:
+                clients[parameters["username"]].send_message(self.data)
+            except:
+                self.send_message(
+                    "{ type: \"error\", error_type: \"User not found\", username: \"" + parameters["username"] + "\" }")
+
+    def connected(self):
+        print(self.address, 'connected')
+        for client in clients.values():
+            client.send_message(self.address[0] + u' - connected')
+        clients[str(self)] = self
+
+    def handle_close(self):
+        # Odstanimo uporabnika izmed prijavljenih
+        for client_key, client_socket in clients.items():
+            if self == client_socket:
+                clients.pop(client_key)
+                break
+
+        print(self.address, 'closed')
+        for client in clients.values():
+            client.send_message(self.address[0] + u' - disconnected')
+
+
+clients = dict()
+
+server = WebSocketServer('', 8000, SimpleChat)
+server.serve_forever()
---
+++ backend/tenorAPI.py
@@ -0,0 +1,35 @@
+# set the apikey and limit
+import json
+import requests
+import sys
+
+apikey = "H75LSQOFMAKG"  # Key za Tenor API
+
+
+def searchForGIFS(numOfGIFs, keywords):
+    gifs = []
+    for search_term in keywords:
+        #print(search_term)
+
+        # get the top noOfGIFs GIFs for the search term
+        r = requests.get(
+            "https://g.tenor.com/v1/search?q=%s&key=%s&limit=%s" % (search_term, apikey, numOfGIFs))
+
+        if r.status_code == 200:
+            # load the GIFs using the urls for the smaller GIF sizes
+            top_gifs = json.loads(r.content)
+            tinyGif = top_gifs["results"][0]["media"][0]["tinygif"]
+            bigGif = top_gifs["results"][0]["media"][0]["gif"]
+            gifs.append([tinyGif, bigGif])
+        else:
+            top_gifs = None
+        # continue a similar pattern until the user makes a selection or starts a new search.
+    return json.dumps(gifs)[1:-1]
+
+
+sys.argv = [2, "Cat"]
+if len(sys.argv) >= 2:
+    ret = searchForGIFS(sys.argv[0], sys.argv[1:])
+    print(ret)
+else:
+    print("Napačna raba API-ja.\nPravilna raba: tenorAPI numOfRequestedGifs keyword,...")
