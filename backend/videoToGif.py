83ca4e5c0e0ba1e47a6e207cfc08958ac8cc76fb
nikigre <nik@nikigre.si>
2021-05-22 16:49:19 +0200

    GIF na katerega lahko neki napišeš

---
+++ backend/videoToGif.py
@@ -0,0 +1,22 @@
+from PIL import Image, ImageDraw, ImageSequence, ImageFont
+import io
+
+im = Image.open('tenor.gif')
+
+# A list of the frames to be outputted
+frames = []
+# Loop over each frame in the animated image
+for frame in ImageSequence.Iterator(im):
+    # Draw the text on the frame
+    d = ImageDraw.Draw(frame)
+    d.text((im.width/2,im.height-25), "Ne vem js zej...",font=ImageFont.truetype("arial.ttf", 20))
+    del d
+
+    b = io.BytesIO()
+    frame.save(b, format="GIF")
+    frame = Image.open(b)
+
+    # Then append the single frame image to a list of frames
+    frames.append(frame)
+# Save the frames as a new image
+frames[0].save('out.gif', save_all=True, append_images=frames[1:])
