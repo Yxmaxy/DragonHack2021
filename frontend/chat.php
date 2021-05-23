a2e944778644d4375228628e93418d135fff4004
Yxmaxy <57218072+Yxmaxy@users.noreply.github.com>
2021-05-23 06:03:50 +0200

    Revert "Neki"
    This reverts commit b90e1231c6fe8a82ac20d447b8a2460966f1a836.

--- frontend/chat.php
+++ frontend/chat.php
@@ -37,31 +37,23 @@ if (!isset($_SESSION['email'])) {
             liha = false;
             obj.forEach(element => {
                 liha = !liha;
-                var url = element[0].url;
-                var bigUrl = element[1].url;
-                var x = element[0].dims[0];
-                var y = element[0].dims[1];
+                var url = element[0].url
+                var x = element[0].dims[0]
+                var y = element[0].dims[1]
                 if (liha) {
                     //div[0].innerHTML += "<img src=\"" + url + "\" width=\"" + x + "px\" height=\"" + y + "px\">"
-                    div[0].innerHTML += "<img onclick='Send(\""+bigUrl+"\");' src=\"" + url + "\">"
+                    div[0].innerHTML += "<img onclick='Send(\""+url+"\");' src=\"" + url + "\">"
                 } else {
                     //div[1].innerHTML += "<img src=\"" + url + "\" width=\"" + x + "px\" height=\"" + y + "px\">"
-                    div[1].innerHTML += "<img onclick='Send(\""+bigUrl+"\");' src=\"" + url + "\">"
+                    div[1].innerHTML += "<img onclick='Send(\""+url+"\");' src=\"" + url + "\">"
                 }
+
             });
         }
 
         socket = null;
         chat_username = null;
         currentLink = "";
-
-        function drawGIF()
-        {
-            const sporocilo1 = document.createElement("div");
-            const ime1 = document.createElement("div");
-            const img1 = document.createElement("img");
-        }
-
 
         function Send(params) {
             currentLink = params;
@@ -193,7 +185,7 @@ if (!isset($_SESSION['email'])) {
         }
 
         function requestGIFS() {
-            var st_slik = 100;
+            var st_slik = 30;
             var GIFkeywords = document.getElementById("searchQuery").value.split(",");
             var obj = {type: "request", numOfGifs: st_slik, keywords: GIFkeywords};
             var text = JSON.stringify(obj)
