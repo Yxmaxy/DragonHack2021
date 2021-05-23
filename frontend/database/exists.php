822f9837618e7ffbb38ac39824295fa816d63c84
nikigre <nik@nikigre.si>
2021-05-23 04:31:11 +0200

    Dodani komentarji

--- frontend/database/exists.php
+++ frontend/database/exists.php
@@ -1,6 +1,7 @@
 <?php
 if(isset($_GET['usernameExists']))
 {
+	//Preveri, če uporabnik s tem uporabniškim imenom že obstaja.
     include "db.php";
     
     $sql="SELECT `ID` FROM `Users` WHERE `UserName`='". mysqli_real_escape_string($conn, $_GET['usernameExists']) ."'";
